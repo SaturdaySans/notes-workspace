1fb37b3e016314810f4c07f27258f9283ee4086c
Saturday <saturdaysansseven@gmail.com>
2026-03-20 22:01:52 +0800

    Changed Frameworks

---
+++ notes-workspace/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Sidebar from "./Sidebar.tsx"
+
+function App() {
+  return (
+    <div className = "flex">
+      <p>Hello world!</p>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
