5d263f791109bc33dd124c3aa84158821543079b
Saturday <saturdaysansseven@gmail.com>
2026-03-23 19:11:27 +0800

    Update Sidebar.tsx

--- notes-workspace/src/Sidebar.tsx
+++ notes-workspace/src/Sidebar.tsx
@@ -1,7 +1,19 @@
 export default function Sidebar() {
     return(
         <aside className = "w-55 h-screen fixed overflow-y-auto bg-[#202020] text-[#bcbab6] p-5 box-border">
-            <p>Sidebar</p>
+            <p className="">General</p>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="pages/search.html">Search</a></li>
+                    <li><a href="pages/inbox.html">Inbox</a></li>
+                </ul>
+                <p>Pages</p>
+                <ul>
+                    <li><a href="pages/physics.html">Physics</a></li>
+                    <li><a href="pages/math.html">Math</a></li>
+                    <li><a href="pages/econs.html">Econs</a></li>
+                    <li><a href="pages/general_paper.html">General Paper</a></li>
+                </ul>
         </aside>
     )
 }
