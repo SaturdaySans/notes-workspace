ff4551983bc5a632a54eadc750088411ff8a91fe
Saturday <saturdaysansseven@gmail.com>
2026-03-24 20:29:30 +0800

    Added lucide react

--- notes-workspace/src/App.tsx
+++ notes-workspace/src/App.tsx
@@ -1,8 +1,18 @@
-import './App.css';
-import Home from "./Home.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Physics from "./pages/Physics";
+import Math from "./pages/Math";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/physics" element={<Physics />} />
+        <Route path="/math" element={<Math />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
